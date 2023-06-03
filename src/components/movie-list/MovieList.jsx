bcd62679dcad9e30b9f652c65e2d336f01ad19be
spectersegun <segunomojolowo@gmail.com>
2023-06-03 23:54:58 +0100

    fixed useEffect lint error

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -35,7 +35,7 @@ const MovieList = (props) => {
       setItems(response.results);
     };
     getList();
-  }, []);
+  }, [props.category, props.id, props.type]);
 
   return (
     <div className='movie-list'>
